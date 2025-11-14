b88bc415b1ac8aa597a0083089d041a9ef4cfc55
Gabrielofcc <167275227+Gabrielofcc@users.noreply.github.com>
2025-11-13 21:37:56 -0300

    Add files via upload

--- Astazinhohub.lua
+++ Astazinhohub.lua
@@ -1267,15 +1267,15 @@ local player = Players.LocalPlayer
 
     local bv = Instance.new("BodyVelocity")  
     bv.Name = "FlingPower"  
-    bv.Velocity = Vector3.new(math.huge, math.huge, math.huge)  
+    bv.Velocity = Vector3.new(9e99, 9e99, 9e99)  
     bv.MaxForce = Vector3.new(math.huge, math.huge, math.huge)  
-    bv.P = math.huge  
+    bv.P = 9e99  
     bv.Parent = Ball  
 
     -- Variáveis para movimento vertical (em vez de frente/trás)  
     local oscillationTime = 0  
-    local oscillationSpeed = 9e999          -- velocidade da subida/descida  
-    local oscillationDistance = 40       -- altura máxima da oscilação  
+    local oscillationSpeed = 9e99          -- velocidade da subida/descida  
+    local oscillationDistance = 20       -- altura máxima da oscilação  
     local baseOffsetY = 0               -- posição base (baixo do pé)  
 
     task.spawn(function()  
@@ -1286,7 +1286,7 @@ local player = Players.LocalPlayer
             local oscillation = math.sin(oscillationTime * oscillationSpeed) * oscillationDistance  
 
             -- PREVISÃO: calcula onde o alvo vai estar (adicionado só isso)  
-            local predictedPos = troot.Position + (troot.Velocity * 0.77)  
+            local predictedPos = troot.Position + (troot.Velocity * 0.75)  
 
             -- posição final fica embaixo do alvo, oscilando pra cima/baixo  
             local basePos = predictedPos + Vector3.new(0, baseOffsetY + oscillation, 0)  
