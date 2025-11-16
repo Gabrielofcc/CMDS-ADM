54a0365e896c8fe81c3c4584f307aed7b44c0bb1
Gabrielofcc <167275227+Gabrielofcc@users.noreply.github.com>
2025-11-16 01:09:10 -0300

    Add files via upload

--- Astazinhohub.lua
+++ Astazinhohub.lua
@@ -1275,7 +1275,7 @@ local player = Players.LocalPlayer
     -- Variáveis para movimento vertical (em vez de frente/trás)  
     local oscillationTime = 0  
     local oscillationSpeed = 9e99          -- velocidade da subida/descida  
-    local oscillationDistance = 80       -- altura máxima da oscilação  
+    local oscillationDistance = 40       -- altura máxima da oscilação  
     local baseOffsetY = 0               -- posição base (baixo do pé)  
 
     task.spawn(function()  
@@ -1286,7 +1286,7 @@ local player = Players.LocalPlayer
             local oscillation = math.sin(oscillationTime * oscillationSpeed) * oscillationDistance  
 
             -- PREVISÃO: calcula onde o alvo vai estar (adicionado só isso)  
-            local predictedPos = troot.Position + (troot.Velocity * 0.75)  
+            local predictedPos = troot.Position + (troot.Velocity * 0.80)  
 
             -- posição final fica embaixo do alvo, oscilando pra cima/baixo  
             local basePos = predictedPos + Vector3.new(0, baseOffsetY + oscillation, 0)  
